d706938824478aea15d15f9888b8e2d8a6243cef
derrix060 <mario_surfing@hotmail.com>
2017-01-20 19:49:14 -0200

    Historical Data generates input

--- src/types/HistoricalData.java
+++ src/types/HistoricalData.java
@@ -1,19 +1,49 @@
 package types;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+
+import types.Data.enumAttributesOfData;
 
 public class HistoricalData {
 	
-	private Map<Calendar, Data> datas = new HashMap<>();
+	private int dateInterval;
+	private ArrayList<Data> mapHistorical;
 
-	public HistoricalData() {
-		// TODO Auto-generated constructor stub
+	public HistoricalData(int dt, ArrayList<Data> historical) {
+		dateInterval = dt;
+		mapHistorical = historical;
 	}
-
-	public Map<Calendar, Data> getDatas() {
-		return datas;
+	
+	
+	
+	public ArrayList<Data> getMapHistorical() {
+		return mapHistorical;
+	}
+	
+	public double[][] toInput(){
+		int elementsQty = mapHistorical.size() - dateInterval;
+		ArrayList<enumAttributesOfData> attr = mapHistorical.get(0).getAttributes();
+		int attrSize = attr.size();
+		int attQty = attrSize * dateInterval;
+		System.out.println("Teste attSize: " + attQty);
+		double[][] rtn = new double[elementsQty][attQty];
+		
+		for (int i=0; i<elementsQty; i++){
+			//for each element (row)
+			
+			for (int j=0; j<dateInterval; j ++){
+				//for each date
+				
+				for (int k=0; k<attrSize; k++){
+					//for each day
+					rtn[i][(j * attrSize) + k] = mapHistorical.get(i+j).getValue(attr.get(k));
+				}
+			}
+		}
+		
+		
+		return rtn;
+		
 	}
 
 }
