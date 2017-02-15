d116efbfa2cc15196c7eb1c89f3607f23933f6f3
derrix060 <mario_surfing@hotmail.com>
2017-02-15 21:51:37 -0200

    implemented CreateNetwork. Need to validade and test

--- src/view/CreateNNBtnAction.java
+++ src/view/CreateNNBtnAction.java
@@ -2,13 +2,16 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 
+import process.NetworkFactory;
 import types.Data.enumAttributesOfData;
+import types.NeuralNetwork;
 
 public class CreateNNBtnAction implements ActionListener {
 	private final NewView view;
@@ -25,13 +28,25 @@ public class CreateNNBtnAction implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+		//attributes
 		List<BasicLayer> layers = view.getLayers();
 		stock = view.getStock();
 		attributes = view.getAtributes();
 		dateInterval = view.getDataInterval();
 		name = view.getName();
+		topology = NetworkFactory.newNetwork(layers);
+		
+		//create NN
+		NeuralNetwork nn = new NeuralNetwork(topology, attributes, dateInterval, stock);
+		
+		//save network
+		try {
+			nn.save(name);
+		} catch (IOException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
 		
 	}
 
