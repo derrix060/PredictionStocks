6d4ecb23041767a14210577f1f5dc543c35ef08f
derrix060 <mario_surfing@hotmail.com>
2017-02-15 22:03:11 -0200

    create TrainNNBtnAction

---
+++ src/view/TrainNNBtnAction.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TrainNNBtnAction implements ActionListener {
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
