2295a5e3cfe07419162c51346054c4d32bc07e87
derrix060 <mario_surfing@hotmail.com>
2017-02-19 19:34:31 -0300

    remove unused imports

--- src/types/NeuralNetwork.java
+++ src/types/NeuralNetwork.java
@@ -2,7 +2,6 @@ package types;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
