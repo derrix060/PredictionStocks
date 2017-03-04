b47eb72caf0f641fb8e8eaa9267a023505d0adc2
derrix060 <mario_surfing@hotmail.com>
2017-03-04 20:27:21 -0300

    remove trash

--- src/view/NewView.java
+++ src/view/NewView.java
@@ -77,10 +77,8 @@ public class NewView extends JFrame {
 			//Network
 				private JTextField txtName;
 				private JTextField txtStock;
-<<<<<<< HEAD
+
 				private NeuralNetwork activeNetwork = null;
-=======
->>>>>>> 3e31e81... add model class diagram
 	
 	
 	
@@ -168,7 +166,7 @@ public class NewView extends JFrame {
 				panelAttr.setBorder(new TitledBorder(null, "Attributes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 				panelAttr.setBounds(244, 11, 107, 160);
 				createPanel.add(panelAttr);
-<<<<<<< HEAD
+
 			
 				
 				chckbxClosePrice.setBounds(6, 26, 96, 23);
@@ -176,9 +174,7 @@ public class NewView extends JFrame {
 				chckbxLowPrice.setBounds(6, 78, 96, 23);
 				chckbxOpenPrice.setBounds(6, 104, 96, 23);
 				chckbxVolume.setBounds(6, 130, 96, 23);
-				
-=======
-				
+		
 				JCheckBox chckbxClosePrice = new JCheckBox("Close Price");
 				JCheckBox chckbxHighPrice = new JCheckBox("High Price");
 				JCheckBox chckbxLowPrice = new JCheckBox("Low Price");
@@ -191,7 +187,6 @@ public class NewView extends JFrame {
 				chckbxOpenPrice.setBounds(6, 104, 96, 23);
 				chckbxVolume.setBounds(6, 130, 96, 23);
 				
->>>>>>> 3e31e81... add model class diagram
 				panelAttr.add(chckbxClosePrice);
 				panelAttr.add(chckbxHighPrice);				
 				panelAttr.add(chckbxLowPrice);				
@@ -204,14 +199,13 @@ public class NewView extends JFrame {
 				createPanel.add(lblDtInterval);
 				
 
-<<<<<<< HEAD
 				txtDataInterval.setBounds(10, 76, 167, 20);
 				createPanel.add(txtDataInterval);
-=======
+
 				JFormattedTextField txtDateInterval = NumberTextField.newField(Integer.class, 1, Integer.MAX_VALUE);
 				txtDateInterval.setBounds(10, 76, 167, 20);
 				createPanel.add(txtDateInterval);
->>>>>>> 3e31e81... add model class diagram
+
 				
 			//Stock
 				JLabel label = new JLabel("Stock");
