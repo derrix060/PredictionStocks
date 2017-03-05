2cb2255330c639af4482df619e782b6991a1867c
derrix060 <mario_surfing@hotmail.com>
2017-03-05 00:01:15 -0300

    create NNPanel to replace NewView

---
+++ src/view/NNPanel.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class NNPanel extends JPanel {
+
+	/**
+	 * Generated serial ID
+	 */
+	private static final long serialVersionUID = 1706251343663324042L;
+	
+	
+	/**
+	 * Constructor
+	 * set Layout to absolute
+	 */
+	public NNPanel() {
+		this.setLayout(null);
+	}
+
+}
