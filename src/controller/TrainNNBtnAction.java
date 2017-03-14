fabfec7846cf603c958c590ffcccf26b9a966e5a
derrix060 <mario_surfing@hotmail.com>
2017-03-14 20:56:39 -0300

    add constructor

--- src/controller/TrainNNBtnAction.java
+++ src/controller/TrainNNBtnAction.java
@@ -3,9 +3,16 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import view.TrainPanel;
+
 public class TrainNNBtnAction implements ActionListener {
 	
+	TrainPanel panel;
 	
+	public TrainNNBtnAction(TrainPanel trainPanel) {
+		this.panel = trainPanel;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
