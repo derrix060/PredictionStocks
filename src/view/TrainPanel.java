a495cc4c3bac804b7644b5b11277db486e7bdca2
derrix060 <mario_surfing@hotmail.com>
2017-03-19 11:15:40 -0300

    improve comments

--- src/view/TrainPanel.java
+++ src/view/TrainPanel.java
@@ -46,80 +46,84 @@ public class TrainPanel extends JPanel {
 		this.view = view;
 		this.setLayout(null);
 		
+		// Dates
+			JLabel lblFrom = new JLabel("From");
+			lblFrom.setBounds(10, 11, 160, 14);
+			this.add(lblFrom);
+			
+			((Component) txtFrom).setBounds(10, 26, 160, 27);
+			this.add((Component) txtFrom);
+			
+			JLabel lblTo = new JLabel("To");
+			lblTo.setBounds(10, 61, 160, 14);
+			this.add(lblTo);
+			
+			((Component) txtTo).setBounds(10, 76, 160, 27);
+			this.add((Component) txtTo);
 		
-		JLabel lblFrom = new JLabel("From");
-		lblFrom.setBounds(10, 11, 160, 14);
-		this.add(lblFrom);
+		// Leraning Rule
+			JLabel lblLearningRule = new JLabel("Learning Rule");
+			lblLearningRule.setBounds(10, 111, 160, 14);
+			this.add(lblLearningRule);
+			
+			cmbLearningRule = new JComboBox<enumTrainingType>();
+			cmbLearningRule.setBounds(10, 130, 160, 20);
+			populateLearningRule();
+			this.add(cmbLearningRule);
 		
-		((Component) txtFrom).setBounds(10, 26, 160, 27);
-		this.add((Component) txtFrom);
+		// Max Iteration and Error
+			JLabel lblMaxIteration = new JLabel("Max Iteration");
+			lblMaxIteration.setBounds(10, 162, 160, 14);
+			this.add(lblMaxIteration);
+			
+			txtMaxIteration = new JFormattedTextField();
+			txtMaxIteration.setBounds(10, 179, 160, 20);
+			this.add(txtMaxIteration);
+			
+			JLabel lblMaxError = new JLabel("Max Error");
+			lblMaxError.setBounds(10, 205, 160, 14);
+			this.add(lblMaxError);
+			
+			txtMaxError = new JFormattedTextField();
+			txtMaxError.setBounds(10, 221, 160, 20);
+			this.add(txtMaxError);
 		
-		JLabel lblTo = new JLabel("To");
-		lblTo.setBounds(10, 61, 160, 14);
-		this.add(lblTo);
+		// Panel Normalization
+			JPanel panelNormalization = new JPanel();
+			panelNormalization.setLayout(null);
+			panelNormalization.setBorder(BorderFactory.createTitledBorder("Normalization"));
+			panelNormalization.setBounds(207, 10, 160, 173);
+			this.add(panelNormalization);
+			
+			JLabel lblInferiorLimit = new JLabel("Inferior Limit");
+			lblInferiorLimit.setBounds(10, 25, 138, 14);
+			panelNormalization.add(lblInferiorLimit);
+			
+			txtInferiorLimit = new JFormattedTextField();
+			txtInferiorLimit.setBounds(10, 40, 138, 20);
+			panelNormalization.add(txtInferiorLimit);
+			
+			JLabel lblSuperiorLimit = new JLabel("Superior Limit");
+			lblSuperiorLimit.setBounds(10, 69, 138, 14);
+			panelNormalization.add(lblSuperiorLimit);
+			
+			txtSuperiorLimit = new JFormattedTextField();
+			txtSuperiorLimit.setBounds(10, 84, 138, 20);
+			panelNormalization.add(txtSuperiorLimit);
+			
+			JLabel lblMargin = new JLabel("Margin %");
+			lblMargin.setBounds(10, 116, 138, 14);
+			panelNormalization.add(lblMargin);
+			
+			txtMargin = new JFormattedTextField();
+			txtMargin.setBounds(10, 130, 138, 20);
+			panelNormalization.add(txtMargin);
 		
-		((Component) txtTo).setBounds(10, 76, 160, 27);
-		this.add((Component) txtTo);
-		
-		JLabel lblLearningRule = new JLabel("Learning Rule");
-		lblLearningRule.setBounds(10, 111, 160, 14);
-		this.add(lblLearningRule);
-		
-		cmbLearningRule = new JComboBox<enumTrainingType>();
-		cmbLearningRule.setBounds(10, 130, 160, 20);
-		populateLearningRule();
-		this.add(cmbLearningRule);
-		
-		JLabel lblMaxIteration = new JLabel("Max Iteration");
-		lblMaxIteration.setBounds(10, 162, 160, 14);
-		this.add(lblMaxIteration);
-		
-		txtMaxIteration = new JFormattedTextField();
-		txtMaxIteration.setBounds(10, 179, 160, 20);
-		this.add(txtMaxIteration);
-		
-		JLabel lblMaxError = new JLabel("Max Error");
-		lblMaxError.setBounds(10, 205, 160, 14);
-		this.add(lblMaxError);
-		
-		txtMaxError = new JFormattedTextField();
-		txtMaxError.setBounds(10, 221, 160, 20);
-		this.add(txtMaxError);
-		
-		JPanel panelNormalization = new JPanel();
-		panelNormalization.setLayout(null);
-		panelNormalization.setBorder(BorderFactory.createTitledBorder("Normalization"));
-		panelNormalization.setBounds(207, 10, 160, 173);
-		this.add(panelNormalization);
-		
-		JLabel lblInferiorLimit = new JLabel("Inferior Limit");
-		lblInferiorLimit.setBounds(10, 25, 138, 14);
-		panelNormalization.add(lblInferiorLimit);
-		
-		txtInferiorLimit = new JFormattedTextField();
-		txtInferiorLimit.setBounds(10, 40, 138, 20);
-		panelNormalization.add(txtInferiorLimit);
-		
-		JLabel lblSuperiorLimit = new JLabel("Superior Limit");
-		lblSuperiorLimit.setBounds(10, 69, 138, 14);
-		panelNormalization.add(lblSuperiorLimit);
-		
-		txtSuperiorLimit = new JFormattedTextField();
-		txtSuperiorLimit.setBounds(10, 84, 138, 20);
-		panelNormalization.add(txtSuperiorLimit);
-		
-		JLabel lblMargin = new JLabel("Margin %");
-		lblMargin.setBounds(10, 116, 138, 14);
-		panelNormalization.add(lblMargin);
-		
-		txtMargin = new JFormattedTextField();
-		txtMargin.setBounds(10, 130, 138, 20);
-		panelNormalization.add(txtMargin);
-		
-		JButton btnTrain = new JButton("Train");
-		btnTrain.setBounds(240, 205, 98, 26);
-		this.add(btnTrain);
-		btnTrain.addActionListener(new TrainNNBtnAction(this));
+		// Train button
+			JButton btnTrain = new JButton("Train");
+			btnTrain.setBounds(240, 205, 98, 26);
+			this.add(btnTrain);
+			btnTrain.addActionListener(new TrainNNBtnAction(this));
 		
 		JPanel panel = new JPanel();
 		panel.setBounds(10, 253, 357, 213);
