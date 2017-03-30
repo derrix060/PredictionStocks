13931ee9c8901376f4cebc66a7b38a5339f498e8
derrix060 <mario_surfing@hotmail.com>
2017-03-29 22:27:04 -0300

    create teste to make tests

---
+++ src/view/_oldTeste.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.util.ArrayList;
+
+public class _oldTeste {
+	
+	public static void main (String args[]){
+		ArrayList<String> palavras = new ArrayList<>();
+		
+		String word = new String();
+		
+		word = "a";
+		
+		palavras.add(word);
+		
+		word = "b";
+		
+		palavras.add(word);
+		
+		System.out.println(palavras);
+	}
+
+}
