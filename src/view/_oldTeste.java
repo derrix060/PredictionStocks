95e704b587ee1edeb49b593232defd34b27c0af7
derrix060 <mario_surfing@hotmail.com>
2017-03-31 18:42:31 -0300

    create test for insert objects in array

--- src/view/_oldTeste.java
+++ src/view/_oldTeste.java
@@ -1,23 +1,39 @@
+
+
+
+
+
+
+
 package view;
-
-import java.util.ArrayList;
-
-public class _oldTeste {
 	
-	public static void main (String args[]){
-		ArrayList<String> palavras = new ArrayList<>();
+	import java.util.ArrayList;
+	
+	public class _oldTeste {
 		
-		String word = new String();
-		
-		word = "a";
-		
-		palavras.add(word);
-		
-		word = "b";
-		
-		palavras.add(word);
-		
-		System.out.println(palavras);
+		public static void main (String args[]){
+			ArrayList<Person> persons = new ArrayList<>();
+			Person person = new Person();
+	
+			person.name = "a";
+			persons.add(person);
+			person.name = "b";
+			persons.add(person);
+			
+			System.out.println(persons);
+		}
 	}
+	
+	class Person{
+		String name;
+		
+		Person(){}
+		
+		@Override
+		public String toString(){
+			return name;
+		}
+	}
+	
 
-}
+
