2553442b1f3ca41ed3a5e2db8c3a028903a99229
Mario Apra <mario_surfing@hotmail.com>
2017-04-09 17:23:41 -0300

    remove unused method from NNFactory

--- src/factories/NetworkFactory.java
+++ src/factories/NetworkFactory.java
@@ -10,34 +10,6 @@ import factories.ActivationFunctionFactory.enumActivationFuncion;
 
 public class NetworkFactory {
 
-	public BasicNetwork getNetwork(List<Integer> layers, List<Boolean> bias, List<Double> dropoutRate, List<enumActivationFuncion> activationFunction) throws IOException{
-		BasicNetwork network = new BasicNetwork();
-		
-		int size = layers.size();
-		
-		//Comparing sizes
-		if (bias.size() != size){
-			throw new IOException("Bias quantity is invalid!");
-		}
-		if (dropoutRate.size() != size){
-			throw new IOException("DropOut quantity is invalid!");
-		}
-		if (activationFunction.size() != size){
-			throw new IOException("ActivationFunction quantity is invalid!");
-		}
-		
-		
-		//create layers
-		for (int i=0; i<layers.size(); i++){
-			network.addLayer(new BasicLayer(ActivationFunctionFactory.create(activationFunction.get(i)), bias.get(i), layers.get(i), dropoutRate.get(i)));
-		}
-		
-		network.getStructure().finalizeStructure();
-		network.reset();
-		
-		return network;
-	}
-	
 	public static BasicNetwork newNetwork(List<BasicLayer> layers){
 		BasicNetwork network = new BasicNetwork();
 		
