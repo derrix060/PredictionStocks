b3a1479813fa35dc3989ef46abc4edb597e78a6d
derrix060 <mario_surfing@hotmail.com>
2017-04-11 22:16:41 -0300

    add test to explain Stream

--- src/view/_oldTeste.java
+++ src/view/_oldTeste.java
@@ -7,14 +7,23 @@
 
 package view;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+
+import model.Data.enumAttributesOfData;
+import model.HistoricalData;
 
 	@SuppressWarnings("unused")	
 	public class _oldTeste {
 		
-		public static void main (String args[]){
+		public static void main (String args[]) throws IOException{
+			
+			exemploStream1();
+			
+			/*
 			ArrayList<Person> persons = new ArrayList<>();
 			Person person = new Person();
 	
@@ -44,8 +53,31 @@ import java.util.List;
 			});
 			
 			santos.forEach(Person::printName);
+			*/
+			
+			
 			
 		}
+		
+		
+		
+		public static void exemploStream1() throws IOException{
+			String ticker = "PETR4.SA";
+			Calendar from = Calendar.getInstance();
+			from.set(2017, Calendar.JANUARY, 12);
+			Calendar to = Calendar.getInstance();
+			to.set(2017, Calendar.MARCH, 24);
+			int dateInterval = 1;
+			ArrayList<enumAttributesOfData> attr = new ArrayList<>();
+			attr.add(enumAttributesOfData.closePrice);
+			attr.add(enumAttributesOfData.highPrice);
+			attr.add(enumAttributesOfData.volume);
+			
+			HistoricalData historical = new HistoricalData(ticker, from, to, dateInterval, attr);
+			
+			historical.getMapHistorical().forEach(System.out::println);
+		}
+		
 	}
 	
 	class Person{
